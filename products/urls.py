0a15f4399a2772b03e7862b9cc08cdf339185330
Simon <rian66746@gmail.com>
2020-03-02 23:47:05 +0600

    Added sell item, update item, delete item

--- products/urls.py
+++ products/urls.py
@@ -2,12 +2,14 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import ProductListView,ProductDetailView,ProductCreateView
+from .views import ProductListView,ProductDetailView,ProductCreateView,ProductUpdateView,ProductDeleteView
 
 urlpatterns = [
     path('',ProductListView.as_view(),name="home"),
     path('product/<int:pk>/',ProductDetailView.as_view(),name="product-detail"),
     path('product/new/',ProductCreateView.as_view(),name="product-create"),
+    path('product/<int:pk>/update',ProductUpdateView.as_view(),name="product-update"),
+    path('product/<int:pk>/delete',ProductDeleteView.as_view(),name="product-delete"),
 ]
 
 if(settings.DEBUG):
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 def home(request):
     context = {'products':Product.objects,'title':"Home"}
@@ -15,10 +16,33 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     fields = ['name','description','image','price']
 
     def form_valid(self,form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Product
+    fields = ['name','description','image','price']
+
+    def form_valid(self,form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        product = self.get_object()
+        if(self.request.user == product.owner):
+            return True
+        return False
+
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Product
+    success_url = '/'
+    def test_func(self):
+        product = self.get_object()
+        if(self.request.user == product.owner):
+            return True
+        return False
